8ae00ffd8e841c11825f5f74f0262652db5e62fa
flodavid <fl.david.53@gmail.com>
2020-03-23 19:38:14 +0100

    First functionnal version
    + Initialization of the game with players
    + Roles assignment
    + Words proposal to the mayor and response handling
    + Game timer, one variable, then one of 1 minute

--- app.js
+++ app.js
@@ -2,16 +2,15 @@
 var Game = require('./game.js');
 var Discord = require('discord.io');
 const jsonToken = require('./token.json');
+var randomWords = require('random-words');
 
-// "discord.io": "github:woor/discord.io#gateway_v6",
 var bot = new Discord.Client({
-    token: jsonToken.token,
-    autorun: true
+  autorun: true,
+  token: jsonToken.token,
 });
 
 bot.on('ready', function() {
   console.log('Logged in as %s - %s\n', bot.username, bot.id);
-  createGame("XXX", 1);
 });
 
 var globalGame;
@@ -26,6 +25,57 @@ function createGame(channelID, duration = 1) {
   // TODO improve to use maps of games and channels
 }
 
+function addPlayer(game, user, userID) {
+  if (!globalGame.tryAddUser(userID)) {
+    bot.sendMessage({
+      to: game.getChannelID(),
+      message: "Sorry, you cannot join the game. Wait for it to finish. (Your are either already in, or it has started)"
+    });
+  } else {
+    bot.sendMessage({
+      to: game.getChannelID(),
+      message: user + " was added to the game. There are "
+      + globalGame.getPlayers().length +" players"
+    });
+  }
+}
+
+function tryElectMayor(channelID) {
+  if (globalGame.hasMayor()) {
+    bot.sendMessage({
+      to: channelID,
+      message: "Choosing a new Mayor... The old one was not good enough"
+    });
+  }
+
+  // Initialize the players
+  let mayor = globalGame.tryInitMayor();
+    console.log("Mayor value is: " + mayor);
+  if (mayor === null) {
+    bot.sendMessage({
+      to: channelID,
+      message: "We need a mayor to play, not enough players"
+    });
+    return false;
+  } else {
+    console.log("Mayor has been elected");
+    bot.sendMessage({
+      to: channelID,
+      message: "Mayor is: <@" + mayor + ">. They must choose a word"
+    });
+    let word1 = randomWords();
+    let word2 = randomWords();
+    let word3 = randomWords();
+    bot.sendMessage({
+      to: mayor,
+      message: "You are the Mayor. Please choose between: " +
+        word1 + ", " + word2 + " and " + word3 + ". Send me back the word you chose\n" +
+        "You can cheat, but that's on you"
+    });
+    return true;
+  }
+}
+
 /**
  * 
  * @param {*} channelID
@@ -37,20 +87,34 @@ function start(channelID) {
   } else {
     bot.sendMessage({
       to: channelID,
-      message: "Starting the game with " + "X" +" players"
+      message: "Starting the game with " + globalGame.getPlayers().length +" players\n" +
+        "You can ask 36 Yes/No questions to the mayor to find the word and/or the werewolf.\n"
     });
-    // Initialize the players
-    let mayor = globalGame.tryInitMayor();
-    client.createDMChannel(mayor);
-    bot.sendMessage({
-      to: channelID,
-      message: "Mayor is: <@" + mayor + ">"
-    });
+    
+    // Elect a mayor instead of starting if there is none
+    if (!globalGame.hasMayor()) {
+      bot.sendMessage({ to: channelID, message: "A mayor must be elected before starting" });
+      if (!tryElectMayor()) {
+        bot.sendMessage({
+          to: channelID,
+          message: "Try to start again when you have enough players" +
+            " (You are " + globalGame.getPlayers().length +" for now)"
+        });
+      }
+    } else {
+      if (!globalGame.hasWerewolf()) {
+        bot.sendMessage({
+          to: channelID,
+          message: "The mayor must choose a word in order for the" +
+            " werewolf to reveal and seer to see"
+        });
+      } else {
+        startGame(channelID, globalGame);
+      }
+    }
 
-    startGame(channelID, globalGame);
   }
 }
-
 
 function startGame(game) {
   let gameStarted = globalGame.tryStart(function() {
@@ -59,28 +123,105 @@ function startGame(game) {
       message: "Time's up Esmeralda. Drop the mic.\n" +
         "You now have one more minute to find the GarouWolf"
       });
-  })
+    this.timerId = setTimeout(function() {
+      bot.sendMessage({
+        to: globalGame.getChannelID(),
+        message: "STOP! Type who you think is the werewolf, **NOW**"
+      });
+    }, 1.1 * (60 * 1000));
+  });
   if (!gameStarted) {
-    bot.sendMessage({ to: globalGame.getChannelID(), message: "Not enough players to start the game" });
-  } else showTimeLeft(globalGame.getChannelID(), game);
+    console.log("Game could not start");
+    bot.sendMessage({
+      to: globalGame.getChannelID(),
+      message: "Not enough players to start the game"
+    });
+  } else {
+    console.log("Game starting...");
+    showTimeLeft(globalGame);
+  }
 }
 
 /**
  * 
- * @param {*} channelID 
- * @param {*} game 
- * Improve to use only channelID ?
+ * @param {*} game
  */
-function showTimeLeft(channelID, game) {
+function showTimeLeft(game) {
   var minutes = Math.floor((game.remainingTime() % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((game.remainingTime() % (1000 * 60)) / 1000);
   
-  bot.sendMessage({ to: channelID, message: "**You have " + minutes + " minutes and " + seconds +" seconds left...**" });
+  bot.sendMessage({
+    to: globalGame.getChannelID(),
+    message: "**You have " + minutes + " minutes and " + seconds +" seconds left...**"
+  });
 }
 
+function handlePrivateMessage(user, userID, message, event) {
+  console.log("I got a private message from: " + user);
+  // TODO add search on all games to find those concerned
+  if (globalGame.isWordSelectByPlayerMessage(userID, message)){
+    bot.sendMessage({
+      to: userID,
+      message: "Secret word: " + globalGame.getSecretWord()
+    });
+    bot.sendMessage({
+      to: globalGame.getChannelID(),
+      message: "The secret word has been chosen! Aoooouuuuh"
+    });
 
-bot.on('message', function(user, userID, channelID, message, event) {
-  console.log("Message: " + message);
+    /*
+    Initialize Werewolf
+     */
+    let werewolf = globalGame.tryInitWerewolf();
+    if (werewolf === null) {
+      bot.sendMessage({
+        to: globalGame.getChannelID(),
+        message: "We need a werewolf to play, not enough players"
+      });
+    } else {
+      // Send the word to werewolf
+      /*
+      Initialize Seer
+      */
+      let seer = globalGame.tryInitSeer();
+      if (seer === null) {
+        bot.sendMessage({
+          to: globalGame.getChannelID(),
+          message: "We need a seer to play, not enough players"
+        });
+      } else {
+        /*
+        Telling each player their role (other players are villagers)
+        */
+        console.log("Werewolf has revealed. Seer can see !");
+        bot.sendMessage({
+          to: globalGame.getChannelID(),
+          message: "Werewolf has revealed. Seer can see !"
+        });
+
+        bot.sendMessage({
+          to: werewolf,
+          message: "You are a werewolf. The secret word is: " + globalGame.getSecretWord()
+        });
+        bot.sendMessage({
+          to: seer,
+          message: "You are the seer. The secret word is: " + globalGame.getSecretWord()
+        });
+
+        globalGame.getVillagers().forEach(function(userID, index, array){
+          bot.sendMessage({
+            to: userID,
+            message: "You are a villager. Try to find the word and/or who is the werewolf"
+          });
+          
+        });
+      }
+    }
+    
+  }
+}
+
+function handlePublicMessage(user, userID, channelID, message, event) {
   if (message.includes("<@!" + bot.id + ">")) {
     bot.sendMessage({
       to: channelID,
@@ -90,59 +231,91 @@ bot.on('message', function(user, userID, channelID, message, event) {
     });
   }
 
-  switch (message) {
+  if (message.startsWith('$')) {
 
-    case "$join":
-      if (!globalGame.tryAddUser(userID)) {  
+    switch (message) {
+
+      case "$join":
+        addPlayer(globalGame, user, userID);
+        break;
+      case "$elect":
+        tryElectMayor(channelID);
+        break;
+      case "$fakegame":
+        addPlayer(globalGame, user, userID);
+        tryElectMayor(channelID);
+        addPlayer(globalGame, "Tata", "Tata");
+        addPlayer(globalGame, "Toto", "Toto");
+        break;
+      case "$start":
+        start(channelID)
+        break;
+      case "$pause":
+        showTimeLeft(globalGame);
         bot.sendMessage({
           to: channelID,
-          message: "Sorry, you cannot join the game. Wait for it to finish. (Your are either already in, or it has started)"
+          message: "Pausing the game"
         });
-      } else {
+        globalGame.pause();
+        break;
+      case "$resume":
         bot.sendMessage({
           to: channelID,
-          message: user + " was added to the game. There are "
-          + globalGame.getPlayers().length +" players"
+          message: "Resuming the game"
         });
-      }
-      break;
-    case "$start":
-      start(channelID)
-      break;
-    case "$pause":
-      showTimeLeft(channelID, globalGame);
-      bot.sendMessage({
-        to: channelID,
-        message: "Pausing the game"
-      });
-      globalGame.pause();
-      break;
-    case "$resume":
-      bot.sendMessage({
-        to: channelID,
-        message: "Resuming the game"
-      });
-      globalGame.resume();
-      showTimeLeft(channelID, globalGame);
-      break;
-    case "$time":
-      showTimeLeft(channelID, globalGame);
-      break;
-    case "$time":
-      showTimeLeft(channelID, globalGame);
-      break;
-    case "$stopall":
-      globalGame = null;
-      bot.sendMessage({
-        to: channelID,
-        message: "The game has been stopped and players removed"
-      });
-      break;
-    case "$help":
-      bot.sendMessage({
-        to: channelID,
-        message: "Available commands are: join, start, pause, time stopall (and help ;))"
-      });
-      break;
-  }
+        globalGame.resume();
+        showTimeLeft(globalGame);
+        break;
+      case "$time":
+        showTimeLeft(globalGame);
+        break;
+      case "$rules":
+        bot.sendMessage({
+          to: channelID,
+          message: "**__Rules of the WhereWordz__**\n" +
+            ">>> - You need at least 4 players (3 will do, but meh)\n" +
+            "- The *Mayor* chooses the word\n" +
+            "- You can ask 36 Yes/No questions to the *Mayor* to find the word and/or the *Werewolf*\n" +
+            "- The *Werewolf* knows the word and must keep the *Villagers* from finding it\n" +
+            "- The *Seer* knows the word and must help the *Villagers*\n" +
+            "- The *Mayor* CAN be the *Werewolf* or *Seer*\n" +
+            "**Victory:**\n" +
+            "- If the word is found at the end the time or questions, the *Werewolf* can win by telling the *Seer*\n" +
+            "- If the word is NOT found, the *Villagers* can win by telling the *Werewolf*\n" +
+            "So both must be careful"
+        });
+        break;
+      case "$stopall":
+        globalGame = null;
+        bot.sendMessage({
+          to: channelID,
+          message: "The game has been stopped and players removed"
+        });
+        break;
+      case "$help":
+        bot.sendMessage({
+          to: channelID,
+          message: "Available commands are: join, elect, start, pause, resume, time, rules, stopall (and help ;))"
+        });
+        break;
+    }
+  }  
+}
+
+bot.on('message', function(user, userID, channelID, message, event) {
+  let duration = 6;
+
+    // TODO create local game instead of global
+    if (typeof globalGame === 'undefined' || globalGame === null) createGame(channelID, duration);
+
+  console.log("Message: " + message);
+  bot.createDMChannel(userID, function(err, res) {
+    // Check if it is private message
+    if (typeof res !== 'undefined' && res.id === channelID) {
+      handlePrivateMessage(user, userID, message, event);
+    } else {
+      handlePublicMessage(user, userID, channelID, message, event);
+    }
+  });
+
 });
--- game.js
+++ game.js
@@ -12,11 +12,12 @@ class Game {
     this.playing = false;
     this.started = false;
     this.users = [];
+    this.villagers = null;
     this.messageCallback = null;
   }
 
   getChannelID() {
-    return channelID;
+    return this.channelID;
   }
 
   getPlayers() {
@@ -29,19 +30,25 @@ class Game {
    */
   tryAddUser(user) {
     if (this.started) {
+      console.log("tryAddUser> Cannot add user, game started");
       return false;
     } else {
       if (this.users.includes(user)) {
+        console.log("tryAddUser> Cannot add user, already in: "+ user);
         return false;
       } else {
         // If there are already villagers, the player is added randomly to them
-        if (this.villagers) {
-          let playerIndex = Math.floor(Math.random() * this.villagers.length + 1);
+        if (this.villagers !== null) {
+          let playerIndex = Math.floor(Math.random() * (this.villagers.length + 1));
+          console.log("tryAddUser> Randomly add user to villagers (" + this.villagers +")," +
+            " may become a werewolf, not a mayor. Index: " + playerIndex);
           // Add the user, removing 0 other villager
-          this.villagers = this.villagers.splice(playerIndex, 0, user)
+          this.villagers.splice(playerIndex, 0, user)
+          console.log("tryAddUser> Villagers: " + this.villagers);
         }
-
+        
         this.users.push(user);
+        console.log("tryAddUser> User added");
         return true; 
       }
     }
@@ -52,11 +59,12 @@ class Game {
    */
   shuffleUsers() {
     // Copy array before randomizing it
-    let shuffledUsers = [...users];
-    var currentIndex = users.length, temporaryValue, randomIndex;
+    let shuffledUsers = [...this.users];
+    var currentIndex = shuffledUsers.length, temporaryValue, randomIndex;
   
     // While there remain elements to shuffle...
     while (0 !== currentIndex) {
+      console.log("Moving 1 item");
   
       // Pick a remaining element...
       randomIndex = Math.floor(Math.random() * currentIndex);
@@ -74,17 +82,84 @@ class Game {
   /**
    * First initialization to do
    */
-  initMayor() {
-    /*
-      * Define a role for every user
-      */
+  tryInitMayor() {
+    if (this.users.length < 1) {
+      console.log("Need at least one player to init mayor");
+      return null;
+    } else {
+      console.log("Initializing the mayor randomly");
 
-    // Randomize the users order and set them as villager by default
-    this.villagers = this.shuffleUsers();
-    // Select the first as mayor and remove it
-    this.mayor = villagers.shift();
+      // Randomize the users order and set them as villager by default
+      this.villagers = this.shuffleUsers();
+      // Select the first as mayor and remove it
+      this.mayor = this.villagers.shift();
 
-    return this.mayor;
+      return this.mayor;
+    }
+  }
+
+  hasMayor() {
+    return typeof this.mayor !== 'undefined' && this.mayor !== null;
+  }
+
+  hasWerewolf() {
+    return typeof this.werewolf !== 'undefined' && this.werewolf !== null;
+  }
+
+  /**
+   * Second/third initialization to do
+   */
+  tryInitWerewolf() {
+    console.log("tryInitWerewolf> Villagers: " + this.villagers)
+    if (this.villagers.length < 1) {
+      console.log("Need at least two players to init werewolf");
+      return null;
+    } else {
+      console.log("Selecting the first villager as werewolf");
+
+      // Select the first as werewolf and remove it
+      this.werewolf = this.villagers.shift();
+      console.log("tryInitWerewolf> Remaining villagers: " + this.villagers);
+
+      return this.werewolf;
+    }
+  }
+
+
+  /**
+   * Second/third initialization to do
+   */
+  tryInitSeer() {
+    console.log("tryInitSeer> Villagers: " + this.villagers)
+    if (this.villagers.length < 1) {
+      console.log("Need at least thee players to init seer");
+      return null;
+    } else {
+      console.log("Selecting the first villager as seer");
+
+      // Select the first as werewolf and remove it
+      this.seer = this.villagers.shift();
+      console.log("tryInitSeer> Remaining villagers: " + this.villagers);
+
+      return this.seer;
+    }
+  }
+
+
+  getVillagers() {
+    return this.villagers;
+  }  
+
+  isWordSelectByPlayerMessage(userID, message) {
+    if (userID == this.mayor) {
+      this.secretWord = message.trim();
+      console.log("The secret word has been set to: "+ this.secretWord);
+      return true;
+    } else return false;
+  }
+
+  getSecretWord() {
+    return this.secretWord;
   }
 
   /**
@@ -92,13 +167,15 @@ class Game {
    */
   tryStart(callback) {
     if (this.users.length < 3) {
-      return null;
+      console.log("Not enough users, not starting");
+      return false;
     } else {
       this.messageCallback = callback;
       this.started = true;
 
       // Start the timer
       this.resume();
+      return true
     }
   }
 
@@ -116,6 +193,7 @@ class Game {
   };
 
   resume() {
+    console.log("Resuming the game");
     this.playing = true;
     this.start = Date.now();
     clearTimeout(this.timerId);
